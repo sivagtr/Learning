45f05664f34ad70b4b115721fb56a7fab09390a6
Siva Srinivasa Rao Mothukuri <sivagtr@gmail.com>
2022-03-14 14:26:39 +0530

    0/1 Knapsack Recursive Approach

---
+++ Algorithms/src/main/java/DynamicProgramming/RecursiveApproach/Knapsack_Zero_One.java
@@ -0,0 +1,52 @@
+package DynamicProgramming.RecursiveApproach;
+
+import java.util.function.Supplier;
+
+public class Knapsack_Zero_One {
+
+    Supplier<Knapsack> knapsackSupplier = ()->{
+        Knapsack knapsack = new Knapsack();
+        knapsack.weights = new int[]{1, 3, 4, 5};
+        knapsack.profits = new int[]{1,3,8,7};
+        knapsack.bagCapacity = 7;
+        return knapsack;
+    };
+
+    public static void main(String[] args) {
+        Knapsack_Zero_One knapsack_zero_one = new Knapsack_Zero_One();
+        knapsack_zero_one.performKnapsack();
+    }
+
+    public void performKnapsack(){
+        Knapsack knapsack = knapsackSupplier.get();
+        int maxProfit = findMaxProfitKnapsack(knapsack.weights, knapsack.profits, knapsack.bagCapacity, knapsack.weights.length);
+        System.out.println("Max Profit is "+maxProfit);
+    }
+
+    private int findMaxProfitKnapsack(int[] weights, int[] profits, int bagCapacity, int length) {
+        /**
+         * Base Condition
+         */
+        if(length == 0 || bagCapacity == 0) return 0;
+        /**
+         * Choice Diagram
+         */
+        // Consider Since weight less than the bag capacity
+        if(weights[length-1] <= bagCapacity){
+            return Math.max(
+                    // Consider that weight
+                    profits[length-1] + findMaxProfitKnapsack(weights, profits, bagCapacity - weights[length-1], length-1),
+                    // Don't consider that weight
+                    findMaxProfitKnapsack(weights, profits, bagCapacity, length-1));
+        }else{
+            // Negative scenario
+            return findMaxProfitKnapsack(weights, profits, bagCapacity, length-1);
+        }
+    }
+
+    private class Knapsack{
+        public int[] weights;
+        public int[] profits;
+        public int bagCapacity;
+    }
+}
