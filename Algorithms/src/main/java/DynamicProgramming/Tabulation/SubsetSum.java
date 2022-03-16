2113359d72ba0a5fb403db750a266efc5959a53f
Siva Srinivasa Rao Mothukuri <sivagtr@gmail.com>
2022-03-16 15:09:02 +0530

    Subset Sum Problem -- Similar to 0/1 Knapsack

--- Algorithms/src/main/java/DynamicProgramming/Tabulation/SubsetSum.java
+++ Algorithms/src/main/java/DynamicProgramming/Tabulation/SubsetSum.java
@@ -1,5 +1,25 @@
 package DynamicProgramming.Tabulation;
 
+/**
+ * Subset Sum problem similar to 0/1 Knapsack.
+ * Here also we have sum as like bag. For Each element, we can either choose to include or exclude it.
+ * So this is similar to knapsack.
+ *
+ * Here we are taking whether the subset sum will be possible or not.
+ * So considering the values as boolean array.
+ *
+ * Initialization
+ * ----------------
+ * If there are NO numbers but sum is present then it is false since we can't achieve that sum
+ * If numbers are empty but sum is 0, it's true
+ *
+ * Only exception case, (0,0) --> Where is true with empty set and sum 0;
+ *
+ * Choice Diagram
+ * ----------------
+ * Similar to knapsack, instead of getting MAX value we can do simple || condition to measure whether
+ * subset can be formed with that sum or not.
+ */
 public class SubsetSum {
     public static void main(String[] args) {
         SubsetSum subsetSum = new SubsetSum();
